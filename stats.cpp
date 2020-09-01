6c401942a233f7af245d6c94bc94cf75eec418a4
deepakkrmnnit <51569452+deepakkrmnnit@users.noreply.github.com>
2020-09-01 13:49:17 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -9,6 +9,26 @@ Stats Statistics::ComputeStatistics(const std::vector<float>& array) {
         s.min = 0.0;
         return s;
     }
-    return arraySizeGreaterThanZero(array);
-    
+    float sum = 0;
+    int count = 0;
+    float max = array[0];
+    float min = array[0];
+    for (unsigned int i = 0;i < array.size();i++)
+    {
+        sum = sum + array[i];
+        count++;
+        if (array[i] > max)
+        {
+            max = array[i];
+        }
+        if (array[i] < min)
+        {
+            min = array[i];
+        }
+    }
+    Stats s;
+    s.average = sum / count;
+    s.max = max;
+    s.min = min;
+    return s;
 }
