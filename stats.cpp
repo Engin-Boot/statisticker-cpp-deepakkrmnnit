7ee9c193779300556658a33331e678b350e6d76b
deepakkrmnnit <51569452+deepakkrmnnit@users.noreply.github.com>
2020-09-01 12:50:32 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -1,15 +1,11 @@
 #include "stats.h"
 
 Stats Statistics::ComputeStatistics(const std::vector<float>& array) {
-    if(array.size()==0)
-    {
         Stats s;
         s.average=0.0;
         s.max=0.0;
         s.min=0.0;
-        return s;
-    }
-    float sum=0;
+        float sum=0;
         int count=0;
         float max=array[0];
         float min=array[0];
@@ -26,7 +22,6 @@ Stats Statistics::ComputeStatistics(const std::vector<float>& array) {
                 min=array[i];
             }   
         }
-        Stats s;
         s.average=sum/count;
         s.max=max;
         s.min=min;
