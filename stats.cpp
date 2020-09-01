52e8c52aa90af20d4818bb177dd955c0f1cb4ebb
deepakkrmnnit <51569452+deepakkrmnnit@users.noreply.github.com>
2020-09-01 12:38:21 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -1,7 +1,7 @@
 #include "stats.h"
 
 Stats Statistics::ComputeStatistics(const std::vector<float>& array) {
-    if(array.size==0)
+    if(array.size()==0)
     {
         Stats s;
         s.average=0.0;
