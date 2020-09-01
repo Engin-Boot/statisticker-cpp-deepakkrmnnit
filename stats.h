b96975c228a7f30c2f9db7ca32ea7cbe3dec9535
deepakkrmnnit <51569452+deepakkrmnnit@users.noreply.github.com>
2020-09-01 13:45:13 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -9,6 +9,33 @@ struct Stats
     
 };
 //
+Stats arraySizeGreaterThanZero(const std::vector<float>&)
+{
+    float sum = 0;
+    int count = 0;
+    float max = array[0];
+    float min = array[0];
+    for (unsigned int i = 0;i < array.size();i++)
+    {
+        sum = sum + array[i];
+        count++;
+        if (array[i] > max)
+        {
+            max = array[i];
+        }
+        if (array[i] < min)
+        {
+            min = array[i];
+        }
+    }
+    Stats s;
+    s.average = sum / count;
+    s.max = max;
+    s.min = min;
+    return s;
+    
+}
+
 namespace Statistics {
     Stats ComputeStatistics(const std::vector<float>& );
 }
