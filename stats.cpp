4a3882aa61e7946901758b5ff408ae28bd711472
deepakkrmnnit <51569452+deepakkrmnnit@users.noreply.github.com>
2020-09-01 14:07:54 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -1,27 +1,47 @@
 #include "stats.h"
-Stats arraySizeGreaterThanZero(const std::vector<float>& array)
+float averageOfArray(const std::vector<float>& array)
 {
     float sum = 0;
     int count = 0;
-    float max = array[0];
-    float min = array[0];
     for (unsigned int i = 0;i < array.size();i++)
     {
         sum = sum + array[i];
         count++;
+    }
+    return sum/count;
+}
+float maxOfArray(const std::vector<float>& array)
+{
+    float max = array[0];
+    for (unsigned int i = 0;i < array.size();i++)
+    {
         if (array[i] > max)
         {
             max = array[i];
         }
+    }
+    return Max;
+}
+float minOfArray(const std::vector<float>& array)
+{
+     float min = array[0];
+    for (unsigned int i = 0;i < array.size();i++)
+    {
         if (array[i] < min)
         {
             min = array[i];
         }
     }
+    return min;
+}
+
+
+Stats arraySizeGreaterThanZero(const std::vector<float>& array)
+{
     Stats s;
-    s.average = sum / count;
-    s.max = max;
-    s.min = min;
+    s.average = averageOfArray(array);
+    s.max = maxOfArray(array);
+    s.min = minOfArray(array);
     return s;
 }
 Stats Statistics::ComputeStatistics(const std::vector<float>& array) {
